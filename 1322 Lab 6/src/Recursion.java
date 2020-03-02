bc62ed46863ef246c38fb0c0f9e81b5d449e4e09
nchri <nchri@192.168.86.30>
2020-03-02 03:50:47 -0500

    1

---
+++ 1322 Lab 6/src/Recursion.java
@@ -0,0 +1,24 @@
+
+public class Recursion {
+	public static void vowels(String x) {
+		int vowelcount = 0;
+		for(int i = 0 ;i < x.length() ;i++) {
+			switch (x.charAt(i)){
+			case 'a': vowelcount++; break;
+			case 'e': vowelcount++; break;
+			case 'i': vowelcount++; break;
+			case 'o': vowelcount++; break;
+			case 'u': vowelcount++; break;
+			case 'A': vowelcount++; break;
+			case 'E': vowelcount++; break;
+			case 'I': vowelcount++; break;
+			case 'O': vowelcount++; break;
+			case 'U': vowelcount++; break;
+			}
+		}
+		System.out.println("Checking the number of vowels in \"" + x +"\" which is: " + vowelcount);
+	}
+public static void main(String[] args) {
+	vowels("Hewo world");
+}
+}
