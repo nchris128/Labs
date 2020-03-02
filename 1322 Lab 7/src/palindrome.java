af6e07eeeb24ad1139aad1e88388a2fc2d718383
chrisngu <nchris128@gmail.com>
2020-03-02 11:26:36 -0500

    finished

--- 1322 Lab 7/src/palindrome.java
+++ 1322 Lab 7/src/palindrome.java
@@ -1,20 +1,28 @@
-
+import java.util.Scanner;
 public class palindrome {
+	
+	public static boolean isPalindrome(String word) {
+	    return word.equals(palindromeTest(word));
+	} 
+	
+	public static String palindromeTest(String word) {
+		  if(word.length() == 1) {
+		    return word;
+		  }
+		  return palindromeTest(word.substring(1)) + word.charAt(0);
+		} 
 
 public static void main(String[] args) {
-	
-	isPalindrome("tacocat");
-	System.out.println(	isPalindrome("taco"));
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Please enter a word: ");
+	String word = sc.nextLine();
+	if (isPalindrome(word) == true) {
+		System.out.println(word + " is indeed a palindrome");
+	}
+	else {
+		System.out.println(word + " is not a palindrome");
+	}
 }
 
-public static boolean isPalindrome(String word) {
-    return word.equals(palindromeTest(word));
-} 
-public static String palindromeTest(String word) {
-	  if(word.length() == 1) {
-	    return word;
-	  }
-	  return palindromeTest(word.substring(1)) + word.charAt(0);
-	} 
 
 }
